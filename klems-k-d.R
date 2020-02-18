6fa627a4e1f34cbbfacee47070420852f99bb56d
rodrigoesborges <rodrigoesborges@gmail.com>
2020-02-18 11:37:07 -0300

    initial tests with data sources - WIOD and EU KLEMS

---
+++ klems-k-d.R
@@ -0,0 +1,33 @@
+#Baixa, explora e processa os dados de KLEMS disponíveis de capital
+
+#Última versão
+#https://euklems.eu/
+
+
+###Maravilha já tem o arquivo no formato R!
+#http://euklems.eu/bulk/Statistical_Capital.rds
+
+#PENÚltima versão
+
+#http://www.euklems.net/TCB/2018/ALL_capital_17i.txt
+
+#Versão de 2007
+#2007 atualizada 2009-2011
+#http://www.euklems.net/data/09i/all_capital_09I.txt
+
+#Inicial
+#http://www.euklems.net/euk07I.shtml
+
+#http://www.euklems.net/data/07i/all_countries_07I.txt
+#http://www.euklems.net/data/07i/all_countries_alt_07I.txt
+
+#Outras KLEMS - a futuro
+#http://www.worldklems.net/data.htm
+#Da América Latina
+#http://laklems.net/
+
+#metodologia de 'serviços de capital' na página 5
+#http://www.euklems.net/data/overview_07i.pdf
+
+
+
--- wiodtests.R
+++ wiodtests.R
@@ -1,8 +1,28 @@
 require("devtools")
 install_github("bquast/wiod")
+install.packages("gvc")
 library("wiod")
+library("gvc")
 library(readxl)
 library(dplyr)
+library(decompr)
+
+#Tabela com taxas de depreciação
+# http://www.wiod.org/publications/source_docs/SEA_Sources.pdf
+
+wiod.list <- list()
+for(this.year in 2000:2014) {
+  wiod.list[[as.character(this.year)]] <- getWIOT(period = this.year,
+                                                     format = "list")
+}
+
+wiod.wide <- list()
+for(this.year in 2000:2014) {
+  wiod.wide[[as.character(this.year)]] <- getWIOT(period = this.year,
+                                                  format = "wide")
+}
+
+wiod1995 <- wiot_1995
 
 sf <- "WIOD_SEA_Nov16.xlsx"
 datadir <- "sourcedata/"
@@ -12,4 +32,10 @@ download.file(seaurl,sf)
 
 sea2016 <- read_xlsx(dfdir,"DATA")
 
-sea2016 %>% filter (variable == "K", grepl("CHL",country))
+chinak <- sea2016 %>% filter (variable == "K", grepl("CHN",country))
+
+chinak <- chinak[,5:length(chinak)] %>% rowSums()
+
+str(wiod.list$`2014`$F)
+
+View(wiod1995$final)
