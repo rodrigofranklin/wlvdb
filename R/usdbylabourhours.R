96d96638319d624b881c8c0597455ddad16266e5
rodrigoesborges <rodrigoesborges@gmail.com>
2020-02-26 19:24:09 -0300

    primera versión con cálculo de valores x sector x país para los años 1995 al 2009, tomando a IDN como representativa del resto de mundo (datos faltantes)
    ==
    primeira versão com cálculo de valores x setor x país para os anos de 1995 a 2009, tomando o IDN como representante do resto do mundo (dados ausentes)
    
    ==
    first version with calculation of values u200bu200bx sector x country for the years 1995 to 2009, taking IDN as representative of the rest of the world (missing data)

--- R/usdbylabourhours.R
+++ R/usdbylabourhours.R
@@ -1,15 +1,111 @@
-##genera vector de las horas totales y año indicado
-if(exists(laborvector)) laborvector <- as.numeric(laborvector)
-else
-  ##Importa SEA de la WIOD v1
-  urlseav1 <- "http://www.wiod.org/protected3/data13/SEA/WIOD_SEA_July14.xlsx"
+library(readxl)
+library(data.table)
+library(tidyr)
+library(dplyr)
+#Para tipos de cambio - verificar a futuro conveniencia de library(wbstats)
+
+library("imfr")
+#ENDA_XDC_USD_RATE
+library(countrycode)
+
+
+
+
+
+# a futuro - convertir en función
+#seadireclabor <- function(ano = 2000, restow = "IDN")
+  #PERMITIR INDICAR 1 PAÍS O varios - por estandar Indonesia
+  # a futuro - establecer los distintos métodos en la misma función
+  #metodo = "paises" | "promedio mundial"
+
+##genera vector de requerimientos directos de trabajo,
+# horas totales por USD para todos los años disponibles
+
+
+
+
+##Importa SEA de la WIOD v1
+
+
+urlseav1 <- "http://www.wiod.org/protected3/data13/SEA/WIOD_SEA_July14.xlsx"
 seav1 <- tempfile()
 download.file(urlseav1,seav1)
-sea95legend <- read_xlsx(seav1, range = "A7:F55", col_names = T)
-sea95 <- read_xlsx(seav1, sheet = "DATA", col_names = T)
-seaano <- sea95[c("Country","Variable","Code",paste0("_",ano))]
-seaano <- data.table(seaano)
-laborvector <- as.matrix(seaano[Variable == "H_EMP" & Code != "TOT", 4])
-laborvector <- as.double(laborvector)
+
+#Importa sin formato la primera hoja con acrónimos - recupera países incluídos en el listado
+# En el momento sirve para el vector de tipos de cambio
+sealegend <- read_xlsx(seav1, range = "A7:F55", col_names = T)
+
+#Importa los datos completos en la hoja
+sea <- read_xlsx(seav1, sheet = "DATA", col_names = T)
+
+#Cambia los nombres de las columnas para después reordenarlas
+colnames(sea) <- c(colnames(sea)[1:4],1995:2011)
+
+#Coloca las columnas de valores en valores numéricos
+sea[,5:ncol(sea)] <- as.numeric(unlist(sea[,5:ncol(sea)]))
+#Futuramente verificar si se han inserido NA's en puntos que
+
+#colocar en formato tidy (columna para valores, columna para el año)
+sea <- sea %>% pivot_longer(cols = -1:-4, names_to = "year", names_ptypes = integer(),
+                            values_to = "value")
+
+sea$year <- as.integer(sea$year)
+
+#Coloca como data.table para dejar el código más compacto a la hora de filtrar columnas
+sea <- data.table(sea)
+
+
+hoursvector <- sea[Variable == "H_EMP" & Code != "TOT"]
+
+vavector <- sea[Variable == "VA" & Code != "TOT"]
+
+
+paiseswiod <- countrycode(unique(sealegend$Name[1:40]), origin = "country.name",destination = "iso2c")
+
+eratevector <- imf_data('IFS','ENDA_XDC_USD_RATE', start = 1995, end = 2011, country = paiseswiod )
+  #ENDA_XDC_USD_RATE
+
+#convertir a iso3c - base de la wiod
+eratevector$Country <- countrycode(eratevector$iso2c, origin = "iso2c", destination = "iso3c")
+
+eratevector$year <- as.integer(eratevector$year)
+
+
+vavector <- vavector %>% left_join(eratevector[,2:4], by = c("Country", "year"))
+
+
+vavector$vausd <- vavector$value/vavector$ENDA_XDC_USD_RATE
+
 #Preparar el vector - completar información ausente - 1400 x 1435
 #seaano[Variable == "H_EMP" & Code != "TOT", ] <- selecciona indonesia
+
+
+directlaboureq <- hoursvector %>% left_join(vavector[,c(1,4,5,8)], by = c("Country","Code","year"))
+
+
+directlaboureq$hourusd <- directlaboureq$value/directlaboureq$vausd
+
+
+### Especifica el RoW como copia de indonesia
+
+rowdle <- directlaboureq[directlaboureq$Country == "IDN",] %>% mutate("Country" = "RoW")
+
+directlaboureq <- rbind(directlaboureq,rowdle)
+
+#Simplifica resultado para quedarse apenas con País, año, sector y horasusd
+
+directlaboureq <- directlaboureq[,c(1,4,5,8)]
+
+#A futuro - comprobar si ya hay estimativa completa con el nombre final del objeto
+#if(exists(laborvector)) 
+#  laborvector <- as.numeric(laborvector)
+#  else
+
+
+
+#Función para importar datos y completarlos de manera a obtener el vector de horas de trabajo /USD
+#Centraliza en el año elegido y la función
+
+
+
+
--- R/wiodvalues.R
+++ R/wiodvalues.R
@@ -1,9 +1,6 @@
-require("devtools")
 library("wiod")
-library(readxl)
 library(dplyr)
 library(matlib)
-library(data.table)
 
 
 
@@ -13,17 +10,20 @@ library(data.table)
 
 
 #Indica los años todos - primera prueba en la primera versión - WIOD2013
-anosv1 <- 1995:2011
+anosv1 <- 1995:2009
 
 #Importa todas las tablas disponibles como 'built-in' en el paquete WIOD
 lapply(anosv1,function(x) do.call(data,as.list(paste0("wiot_",x))))
 
-laborvaluesv1 <- function(ano = 2000, intermed , laborvector) {
+#importa SEA y completa los datos de horas trabajadas
+source("usdbylabourhours.R")
+
+laborvaluesv1 <- function(ano = 2000) {
   ####1) Coeficientes Técnicos
-  intermed <- as.array(intermed)
-  matident <- diag(length(coef_tec))
+  intermed <- as.array(get(paste0("wiot_",ano))$inter)
   coef_tec <- prop.table(intermed,2)
   coef_tec <- as.matrix(coef_tec)
+  matident <- diag(nrow(coef_tec))
   ####2)Inversa de Leontief
   
   ainverter <- matident - coef_tec
@@ -31,11 +31,12 @@ laborvaluesv1 <- function(ano = 2000, intermed , laborvector) {
   leontief <- solve(ainverter)
   
   ####3) Vector de coeficientes de trabajo por setor - en script separado - distintas posibilidades
-  source("usdbylabourhours.R")
-    
+  #  source("usdbylabourhours.R") más arriba
+    laborvector <- directlaboureq[directlaboureq$year == ano,4]
   #4) multiplicar vector de requerimientos directos trabajo por inversa de leontief
   
     #horas-trabajo / producción bruta
-  laborvalues <- laborvector * leontief
+  laborvalues <- laborvector ** leontief
 }
 
+laborvaluesv1()
