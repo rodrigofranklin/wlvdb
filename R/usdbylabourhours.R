8fc1adeed45d189bb65bdfccb6bce5d50c0693b1
rodrigoesborges <rodrigoesborges@gmail.com>
2020-02-28 12:16:16 -0300

    correciones y claridad de código para el calculo del vector de requerimientos directos de trabajo

--- R/usdbylabourhours.R
+++ R/usdbylabourhours.R
@@ -24,69 +24,37 @@ library(countrycode)
 
 
 
-##Importa SEA de la WIOD v1
-
-
-urlseav1 <- "http://www.wiod.org/protected3/data13/SEA/WIOD_SEA_July14.xlsx"
-seav1 <- tempfile()
-download.file(urlseav1,seav1)
-
-#Importa sin formato la primera hoja con acrónimos - recupera países incluídos en el listado
-# En el momento sirve para el vector de tipos de cambio
-sealegend <- read_xlsx(seav1, range = "A7:F55", col_names = T)
-
-#Importa los datos completos en la hoja
-sea <- read_xlsx(seav1, sheet = "DATA", col_names = T)
-
-#Cambia los nombres de las columnas para después reordenarlas
-colnames(sea) <- c(colnames(sea)[1:4],1995:2011)
-
-#Coloca las columnas de valores en valores numéricos
-sea[,5:ncol(sea)] <- as.numeric(unlist(sea[,5:ncol(sea)]))
-#Futuramente verificar si se han inserido NA's en puntos que
-
-#colocar en formato tidy (columna para valores, columna para el año)
-sea <- sea %>% pivot_longer(cols = -1:-4, names_to = "year", names_ptypes = integer(),
-                            values_to = "value")
-
-sea$year <- as.integer(sea$year)
-
-#Coloca como data.table para dejar el código más compacto a la hora de filtrar columnas
-sea <- data.table(sea)
-
+source("R/seaimport.R")
 
 hoursvector <- sea[Variable == "H_EMP" & Code != "TOT"]
 
 #más clareza en nomenclatura
 colnames(hoursvector)[6] <- "hours"
 
-vavector <- sea[Variable == "VA" & Code != "TOT"]
+
+#combina países y sectores
+pasect <- expand.grid(sectors = wiot_1995$industries, stringsAsFactors = F, countries = wiot_1995$countries)
 
 
-paiseswiod <- countrycode(unique(sealegend$Name[1:40]), origin = "country.name",destination = "iso2c")
+#obtiene el producto total de las wiot, ordenados año a año
 
-eratevector <- imf_data('IFS','ENDA_XDC_USD_RATE', start = 1995, end = 2011, country = paiseswiod )
-  #ENDA_XDC_USD_RATE
+paisecvtan <- function (ano = 2000) {
+  wtbase <- get(paste0("wiot_",ano))
+  cbind(year = wtbase$year,pasect,output = wtbase$output)
+  }
 
-#convertir a iso3c - base de la wiod
-eratevector$Country <- countrycode(eratevector$iso2c, origin = "iso2c", destination = "iso3c")
+vtotvector <-  rbindlist(lapply(anosv1, paisecvtan))
 
-eratevector$year <- as.integer(eratevector$year)
-
-
-vavector <- vavector %>% left_join(eratevector[,2:4], by = c("Country", "year"))
-
-
-vavector$vausd <- 1000000*vavector$value/vavector$ENDA_XDC_USD_RATE
-
+#define nombres de columnas como los de SEA por claridad de código 
+names(vtotvector) <- c("year","Code","Country","Valor Total")
 #Preparar el vector - completar información ausente - 1400 x 1435
 #seaano[Variable == "H_EMP" & Code != "TOT", ] <- selecciona indonesia
 
 
-directlaboureq <- hoursvector %>% left_join(vavector[,c(1,4,5,8)], by = c("Country","Code","year"))
+directlaboureq <- hoursvector %>% left_join(vtotvector, by = c("Country","Code","year"))
 
-
-directlaboureq$hourusd <- directlaboureq$hours/directlaboureq$vausd
+#mantener en millón de horas
+directlaboureq$hourusd <- directlaboureq$hours/directlaboureq$`Valor Total`
 
 #troca NA por 0 - setores sem informação de produção de valor
 directlaboureq[is.na(directlaboureq$hourusd),8] <- 0
