a4458195605fa620ebf994fd18660c58c6e2be08
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2020-04-15 16:47:48 -0500

    Beginning the modularization of the two main scripts TransformarTudo and Transformar.R
    inicio de la modularización de los dos scripts principales - TransformarTudo y Transformar.R
    
    início da modularização dos dois scripts principais TransformarTudo e Transformar.R

--- R/TransformarTudo.R
+++ R/TransformarTudo.R
@@ -10,26 +10,9 @@ library(beepr)
 #VERSAO <- 'July14'
 VERSAO <- 'Nov16'
 
-# Lista dos países
-Paises <- read.csv2(file = paste0(getwd(),"/sourcedata/",VERSAO,"/Paises.csv"), row.names = 1)
-Num_Paises <- dim(Paises)[1]
+#Paises y sectores - Inicialmente LinProds ColProds PaisLins pasado a módulo propio
+source('R/sectorespaises.R')
 
-# Obtém a informação dos setores produtivos e prepara as variávels LinProds, ColProds e PaisLins
-Setores<-read.csv2(paste0(getwd(),"/sourcedata/",VERSAO,"/setores.csv"))
-Num_Setores <- dim(Setores)[1]
-LinProds <- NULL
-PaisLins <- NULL
-W=1
-for (X in Paises[,2]) {
-  for (Y in 1:Num_Setores){
-    PaisLins <- c(PaisLins, X)
-    if (Setores[Y,4]==1)  {LinProds <- c(LinProds,W)}
-    W<-W+1
-  }
-}
-LinProds <- t(LinProds)
-ColProds <- LinProds
-LinConsumoIntermediario <- Num_Paises*Num_Setores + 1
 
 # Obtém informações sobre as colunas de demanda
 Demanda<-read.csv2(paste0(getwd(),"/sourcedata/",VERSAO,"/demanda.csv"))
