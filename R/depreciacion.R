2ad00aed5806a192d6f75c831f4a364abbec1b8a
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2020-05-14 14:54:32 -0500

    error tipográfico en comentario

--- R/depreciacion.R
+++ R/depreciacion.R
@@ -1,5 +1,5 @@
 #K Depreciation - Depreciación de Capit
-#version inicial, sin depreciació
+#version inicial, sin depreciación
 #remover ) and -Depreciation)
 depreciacion <- matrix(0, nrow = length(LinProds), ncol = length(ColProds))
    
