e910aa5147ba6a4e1451bc943d38dcf198b6dc55
Rodrigo E. S. Borges <rodrigo@borges.net.br>
2020-07-17 18:34:34 -0300

    cambio de extensiones de archivos para minúsculas
    en
    change file extensions to lowercase
    
    pt
    alterar extensões de arquivos para minúsculas

--- R/euklems/todo_kelms.R
+++ R/euklems/todo_kelms.R
@@ -3,6 +3,6 @@
 
 for (ano in 1995:2015) {
   source("R/euklems/k-prop.R")
-  saveRDS(ek.k, file = paste0("sourcedata/euklems/ekk_",ano,".RDS"))
-  saveRDS(ek.tx.dep, file = paste0("sourcedata/euklems/ektxdep_",ano,".RDS"))
+  saveRDS(ek.k, file = paste0("sourcedata/euklems/ekk_",ano,".rds"))
+  saveRDS(ek.tx.dep, file = paste0("sourcedata/euklems/ektxdep_",ano,".rds"))
 }
