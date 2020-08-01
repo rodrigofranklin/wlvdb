0d17733be9b0a540dcebafc9b2aa14edc2287f82
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2020-08-01 16:18:51 -0500

    == es creación de scripts para tabla de rotación (en función de método elegido) y cálculo de precios de producción según método de Ochoa, 1984
    ==
    en
    
    Creation of scripts for turnover-table (function of chosen method) and estimates of price of production following Ochoa, 1984.
    
    ==
    pt
    
    Criação de scripts para tabela de rotação (em função de método escolhido) e cálculo de preços de produção segundo o método de Ochoa, 1984

---
+++ R/precios_de_produccion.R
@@ -0,0 +1,5 @@
+#### Estima los precios de producción según los datos obtenidos de TransformarTudo y tabla de rotación estimada.
+
+#source("R/TransformarTudo.R")
+source("R/turnover-rotacion.R")
+t <- rotacion()
