b2195d98a1cfd538a9f8bee926164538095e319d
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2020-08-15 14:44:07 -0500

    == es
    inicio de la recopilación de la matriz B
    
    ==
    pt
    início da compilação da matriz B
    
    ==
    en
    
    Beggining compilation of B matrix

--- R/precios_de_produccion.R
+++ R/precios_de_produccion.R
@@ -13,23 +13,38 @@ t <- rotacion()
 # ao <- requerimientos directos laborales
 # b'.ao <- requerimientos directos laborales de la cesta de consumo obrera
 
-#### El problema es de máximo autovalor para auto-vector positivo (único que tiene sentido económico, precios >0) de la siguiente matriz:
+#### El problema es de máximo autovalor para auto-vector con valores todos positivos
+#(único que tiene sentido económico, precios >0) de la siguiente matriz:
 
 #[K + (A +b.ao)<t>] (I - A - b'.ao - D)-1
 
+##Ever y César proponen forma de calcular B distinta de b.ao , a falta de b
+
+##Directamente de tablas insumo producto. Pasos
+#1) Proporción del consumo de hogares representado por salarios
+#del codigo principal
+# Estimativa de variáveis para o RoW
+
+#  col.demanda.final <- consumo final de hogares <-retirar completa
+salarios <- sea[sea$variable == 'LAB' & sea$code %in% setores[setores$produtivo ==1,"Code"], "2014"]
+##Hace falta incluir a ROW
+#source(paste0(getwd(),"/R/suposicoes_row.R"))
+
+
 #H transpuesta
 #H = [M(I-N)^-1]
-#M = A + D + B
+#M = A (coeficientes) + D (m.depreciacao) + B
 #M = K + A + B
   
 #matriz.b <- 
 #matriz.m <- coeficientes+m.depreciacao+matriz.b
 
-matriz.n <- k.composicao+coeficientes+matriz.b
+#matriz.n <- k.composicao+coeficientes+matriz.b
 
-matriz.h <- solve(matriz.m*(diag(1,nrow = cols)-matriz.n))
+#matriz.h <- solve(matriz.m*(diag(1,nrow = cols)-matriz.n))
 
-matriz.ht <- transpose(matriz.h)
+#matriz.ht <- transpose(matriz.h)
 
-pre_prod <- eigen(matriz.ht)
+#pre_prod <- eigen(matriz.ht)
 
+unique(sea$country)
