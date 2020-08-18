988dd899c24faee0b4495829220da0d1fb92eb31
Rodrigo E. S. Borges <rodrigo@borges.net.br>
2020-08-18 00:09:16 -0300

    == es
    ejemplo revisado y arreglado, inserido método de Franklin/Borges (dist cons. según proporciones de salarios sectoriales)
    
    Pendiente - aparentemente problema con matriz de K de Brasil
    
    ==
    pt
    
    Exemplo revisado e consertado, método de Franklin / Borges inserido (cons. Dist. De acordo com as proporções salariais setoriais)
    
    Problema aparentemente pendente com a matriz K do Brasil
    
    ==
    en
    
    Revised and fixed example, Frankli/n Borges method inserted (H.cons. Dist. According to sectorial wage proportions)
    
    Problem apparently pending with matrix K from Brazil

--- R/p_p_brasil_2014_ex.R
+++ R/p_p_brasil_2014_ex.R
@@ -9,7 +9,7 @@ library(pracma)
 wiot.bra <- wiot %>% filter(Year == 2014) %>% select(1:5,starts_with("BRA"))
 wiot.bra <- wiot.bra %>% filter(Country == "BRA")
 
-wiot.bra <- wiot.bra[,-1:-5]
+#wiot.bra <- wiot.bra[,-1:-5]
 
 
 
@@ -68,10 +68,16 @@ x <- 5
     }
     w <- w+1
   }
-}
 
 
+##cambio
+cambio.bra <- cambio[seq_completos]
 
+  
+
+
+sec_prod <- as.logical(setores$produtivo)
+  
 inicio<-4*32+1
 
 seq_inicio<- (4*32+1):(5*32)
@@ -83,33 +89,38 @@ d.bra <- depreciacao[seq_inicio,seq_inicio]
 
 seq_completos <- (56*4+1):(56*5)
 
-sec_k_bra <- grepl("BRA",dimnames(k.composicao)[[2]])
+sec_k_bra <- which(grepl("BRA",dimnames(k.composicao)[[2]]))
 k.bra.completa <- k.composicao[sec_k_bra,sec_k_bra]
 
-k.bra.completa <- matrix(k.bra.completa, nrow = 56,ncol = 56)
+#k.bra.completa <- matrix(k.bra.completa, nrow = 56,ncol = 56)
 
 k.bra <- k.bra.completa[sec_prod, sec_prod]
 
 k.bra[is.na(k.bra)] <- 1
 
 
+#SE cambio já está?
+k.bra.us <- cambio.bra[1]*k.bra
+
 ###Vamos a construir B
 
-sec_prod <- as.logical(setores$produtivo)
+
 
 
 sea.bra <- sea.tidy %>% filter(country == "BRA", year == 2014)
 
 sal.bra <- sea.bra %>% filter(variable == "LAB") %>% select(value)
 
-##cambio
-cambio.bra <- cambio[seq_completos]
-sal.bra <- sal.bra*cambio.bra
+price_level <- sea.bra %>% filter(variable == "VA_P"|variable == "VA_PI") %>% select(value)
 
+sal.bra <- sal.bra*100/price_level
+sal.bra <- sal.bra*cambio.bra
 
 #sal.bra <- sal.bra[which(sec_prod),]
 
+sal.bra <- lab.usd[seq_completos]
 
+sal.bra <- sal.bra[sec_prod]
 #sal.bra <- t(sal.bra)
 ###Consumo de hogares
 
@@ -124,15 +135,23 @@ cons.hog.bra.prod <- cons.hog.bra.comp[sec_prod]
 cx <- sum(sal.bra)/sum(cons.hog.bra.comp)
 
 
-### tcw  h_emp/sum(h_emp)   
+### tcw  h_emp/sum(h_emp)    <<--metodo CESAR/EVER
 
-h.emp.bra <- h.emp[seq_completos]
+#h.emp.bra <- h.emp[seq_completos]
 
-tcw.bra <- h.emp.bra/sum(h.emp.bra)
+#tcw.bra <- h.emp.bra/sum(h.emp.bra)
 
-tcw.bra <- tcw.bra[sec_prod]
+#tcw.bra <- tcw.bra[sec_prod]
 
-tcw.bra <- tcw.bra/sum(tcw.bra)
+#tcw.bra <- tcw.bra/sum(tcw.bra)
+
+# tcw sal.bra/sum(sal.bra) <<-- metodo Franklin/Rodrigo
+#Utilizar salarios en lugar de horas trabajadas
+tcw.bra <- sal.bra/sum(sal.bra)
+
+#Ya está subseleccionado
+#tcw.bra.sal <- tcw.bra.sal[sec_prod]
+
 #### consumo de trabajadores
 c <- cx*cons.hog.bra.prod
 
@@ -141,30 +160,34 @@ c <- cx*cons.hog.bra.prod
 E <- c%*%t(tcw.bra)
 
 ### B - coeficientes de bienes salarios (E) por unidad de producción bruta
+seq_iot <- (61*4+1):(61*5-5)
+wiot.bra.go <-  wiot %>% 
+  filter(IndustryCode == "GO", Country== "TOT",Year == 2014) 
 
-wiot.bra.go <-  wiot %>% filter(IndustryCode == "GO") %>% select(-1:-5)
+wiot.bra.go <- as.numeric(t(wiot.bra.go)[which(grepl("BRA",dimnames(wiot.bra.go)[[2]]))])
 
-wiot.bra.go <- wiot.bra.go[seq_completos]
+wiot.bra.go <- wiot.bra.go[1:56]
 
-wiot.bra.go <- wiot.bra.go[sec_prod]
+wiot.bra.go <- wiot.bra.go[which(sec_prod)]
+
+#wiot.bra.go <- as.data.frame(wiot.bra.go)[,sec_prod]
 
 ##Evitar división por 0
 wiot.bra.go[wiot.bra.go == 0] <- 1
 
-B <- E%*%solve(diag(wiot.bra.go))
+#wiot.bra.go <- as.data.frame(wiot.bra.go)
 
+B <- E%*%solve(diag(wiot.bra.go))
 
 ##Matriz de Capital <- dividida por producción bruta
 
-k.bra.go <- mapply("/",data.frame(t(k.bra)),t(wiot.bra.go))
+k.bra.go <-(matrix(mapply("/",t(k.bra.us),(wiot.bra.go)),
+                  nrow=sum(sec_prod),byrow = T))
 
-k.bra.go.loc <- k.bra.go*max(cambio.bra)
-
-
-M <- coeficientes.bra+k.bra.go.loc+B
+M <- a.bra+k.bra.go+B
 
 
-N <- coeficientes.bra+B+d.bra
+N <- a.bra+B+d.bra
 
 H <- M*solve(diag(1,32)-N)
 
@@ -172,5 +195,5 @@ prec_prod <- eigen(t(H))
 
 #max(prec_prod$values)
 
-1/prec_prod$values[1]
+1/as.double(prec_prod$values[1])
 
