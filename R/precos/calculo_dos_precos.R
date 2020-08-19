52ec24e22e519c27ed4cc9cf6f7b65cdb44a63de
rodrigo.franklin.br <rodrigo.franklin.br@gmail.com>
2020-08-19 19:37:01 -0300

    Acrescentando preços de produção

--- R/precos/calculo_dos_precos.R
+++ R/precos/calculo_dos_precos.R
@@ -7,6 +7,42 @@ precos_mercado <- m.wio[lin.produto.total, 1:tamanho]
 Cols <- max(pais.cols[])
 k_n = ProdutoTotalMPais = ProdutoTotalTPais <- matrix(0,1,Cols)
 
+#[K + (A + b.a0)<t>] (I - A - b.a0 - D)-1
+
+A <- m.wio[1:tamanho,1:tamanho]/matrix(m.wio[lin.produto.total,1:tamanho], nrow = tamanho, ncol=tamanho, byrow = TRUE)
+A[is.infinite(A)] <- 0
+A[is.nan(A)] <- 0
+
+K <- k.composicao/matrix(m.wio[lin.produto.total,1:tamanho], nrow = tamanho, ncol=tamanho, byrow = TRUE)
+K[is.infinite(K)] <- 0
+K[is.nan(K)] <- 0
+
+D <- m.depreciacao/matrix(m.wio[lin.produto.total,1:tamanho], nrow = tamanho, ncol=tamanho, byrow = TRUE)
+D[is.infinite(D)] <- 0
+D[is.nan(D)] <- 0
+
+t <- diag(tamanho)
+t[is.infinite(t)] <- 0
+t[is.nan(t)] <- 0
+
+I <- diag(tamanho)
+
+prop_demanda_familias <- as.data.frame(prop.table(m.wio[1:tamanho,col.demanda.final], margin = 2))
+prop_demanda_familias <- as.matrix(prop_demanda_familias[rep(names(prop_demanda_familias), each = num.setores)])
+
+b.a0 <- (matrix(sea["lab.usd",], ncol = tamanho, nrow = tamanho, byrow = TRUE)*prop_demanda_familias)/matrix(m.wio[lin.produto.total,1:tamanho], nrow = tamanho, ncol=tamanho, byrow = TRUE)
+b.a0[is.infinite(b.a0)] <- 0
+b.a0[is.nan(b.a0)] <- 0
+
+#[K + (A + b.a0)<t>] (I - A - b.a0 - D)-1
+prec_prod <- eigen(t((K + (A+b.a0)%*%t ) * solve(I -A -b.a0 -D)))
+
+ro <- 1/Re(prec_prod[["values"]][1])
+prec_prod <- Re(prec_prod[["vectors"]][,1])
+
+k <- sum(m.wio[lin.produto.total,1:tamanho])/sum(m.wio[lin.produto.total,1:tamanho]*prec_prod)
+prec_prod <- k*prec_prod*m.wio[lin.produto.total,1:tamanho]
+
 for (x in 1:num.paises){
   #Produto total em horas de trabalho e em moeda (soma dos setores produtivos)
   ProdutoTotalTPais[x] <- sum(m.t[lin.produto.total, col.prods[which(pais.cols[col.prods]==x)]])
--- R/precos/precos_diretos.R
+++ R/precos/precos_diretos.R
@@ -1,14 +1,25 @@
 # Script para leitura dos preços diretos, preços de produção e valores
 
-versao = versao_fonte <- "July14"
-versao_resultado <- "July14_1006"
-anos <- 1995:2009
-# versao = versao_fonte <- "Nov16"
-# versao_resultado <- "Nov16_1007"
-# anos <- 2000:2014
+# versao = versao_fonte <- "July14"
+# versao_resultado <- "July14_1006"
+# anos <- 1995:2009
+versao = versao_fonte <- "Nov16"
+versao_resultado <- "Nov16_1007"
+anos <- 2000:2014
+
+source("R/turnover-rotacion.R")
 
 source('R/sectorespaises.R')
 pais.cols <- pais.lins
+tamanho <- num.paises*num.setores
+
+# Obtém informações sobre as colunas de demanda das famílias
+demanda<-read.csv2(paste0(getwd(),"/sourcedata/",versao,"/demanda.csv"))
+num.demanda <- dim(demanda)[1]
+col.demanda.final <- NULL
+for (x in paises[,2]) {
+  col.demanda.final <- c(col.demanda.final, (tamanho) + demanda[demanda == 'Final consumption expenditure by households',3] + (num.demanda*(x-1)))
+}
 
 paises.setores <- data.frame(country=rep(paises$Legenda,each=num.setores))
 paises.setores$description <- setores$Setor
@@ -23,26 +34,36 @@ resultado_temp$variable <- 'precos_diretos_n'
 resultado <- rbind(resultado, resultado_temp)
 resultado_temp$variable <- 'precos_mercado'
 resultado <- rbind(resultado, resultado_temp)
+resultado_temp$variable <- 'precos_producao'
+resultado <- rbind(resultado, resultado_temp)
+
+taxa_lucro_media_mundo <- NULL
 
 for (ano in anos) {
   if (versao == "July14") {
     m.wio <- readRDS(paste0(getwd(),"/sourcedata/",versao,"/WIOT_",as.character(ano),".rds"))
     m.t <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/wiod_horas_",as.character(ano),".rds"))
+    k.composicao <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/k_composicao_",as.character(ano),".rds"))
+    m.depreciacao <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/m_depreciacao_",as.character(ano),".rds"))
+    sea <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/socioeconomicas_",as.character(ano),".rds"))
     lin.produto.total <- nrow(m.wio)
   } else {
     load(paste0(getwd(),"/sourcedata/Nov16/WIOT",as.character(ano),"_October16_ROW.RData"))
     m.wio <- as.matrix(wiot[,6:ncol(wiot)])
     m.t <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/wiod_horas_",as.character(ano),".rds"))
+    k.composicao <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/k_composicao_",as.character(ano),".rds"))
+    m.depreciacao <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/m_depreciacao_",as.character(ano),".rds"))
+    sea <- readRDS(paste0(getwd(),"/resultados/",versao_resultado,"/socioeconomicas_",as.character(ano),".rds"))
     lin.produto.total <- which(wiot[,'IndustryCode'] == 'GO')
   }  
   
-  tamanho <- num.paises*num.setores
   source('R/precos/calculo_dos_precos.R')
   
-  resultado$temp <- t(cbind(t(valores), t(precos_diretos), t(precos_diretos_n), t(precos_mercado)))
+  resultado$temp <- t(cbind(t(valores), t(precos_diretos), t(precos_diretos_n), t(precos_mercado), t(prec_prod)))
   names(resultado)[names(resultado) == "temp"] <- ano
+  taxa_lucro_media_mundo <- c(taxa_lucro_media_mundo, ro)
 }
 
 write.csv2(resultado, file = paste0("resultados/",versao_resultado,"/precos_",versao_resultado,".csv"))
-
+write.csv2(taxa_lucro_media_mundo, file = paste0("resultados/",versao_resultado,"/tx_lucro_",versao_resultado,".csv"))
 
