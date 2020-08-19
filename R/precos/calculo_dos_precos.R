9a4cda8fc76c8ecc157f56d19f8d15ca7fd7e265
rodrigo.franklin.br <rodrigo.franklin.br@gmail.com>
2020-08-19 20:32:05 -0300

    Ajustes nos cálculos dos preços

--- R/precos/calculo_dos_precos.R
+++ R/precos/calculo_dos_precos.R
@@ -1,14 +1,11 @@
+#### Valores, preços de mercado e preços diretos
 valores <- m.t[lin.produto.total, 1:tamanho]
-precos_mercado <- m.wio[lin.produto.total, col.prods]
+precos_mercado <- m.wio[lin.produto.total, 1:tamanho]
 k <- sum(precos_mercado)/sum(valores)
 precos_diretos <-  k * valores
-precos_mercado <- m.wio[lin.produto.total, 1:tamanho]
 
-Cols <- max(pais.cols[])
-k_n = ProdutoTotalMPais = ProdutoTotalTPais <- matrix(0,1,Cols)
-
+#### Preços de produção
 #[K + (A + b.a0)<t>] (I - A - b.a0 - D)-1
-
 A <- m.wio[1:tamanho,1:tamanho]/matrix(m.wio[lin.produto.total,1:tamanho], nrow = tamanho, ncol=tamanho, byrow = TRUE)
 A[is.infinite(A)] <- 0
 A[is.nan(A)] <- 0
@@ -29,7 +26,6 @@ I <- diag(tamanho)
 
 prop_demanda_familias <- as.data.frame(prop.table(m.wio[1:tamanho,col.demanda.final], margin = 2))
 prop_demanda_familias <- as.matrix(prop_demanda_familias[rep(names(prop_demanda_familias), each = num.setores)])
-
 b.a0 <- (matrix(sea["lab.usd",], ncol = tamanho, nrow = tamanho, byrow = TRUE)*prop_demanda_familias)/matrix(m.wio[lin.produto.total,1:tamanho], nrow = tamanho, ncol=tamanho, byrow = TRUE)
 b.a0[is.infinite(b.a0)] <- 0
 b.a0[is.nan(b.a0)] <- 0
@@ -43,17 +39,15 @@ prec_prod <- Re(prec_prod[["vectors"]][,1])
 k <- sum(m.wio[lin.produto.total,1:tamanho])/sum(m.wio[lin.produto.total,1:tamanho]*prec_prod)
 prec_prod <- k*prec_prod*m.wio[lin.produto.total,1:tamanho]
 
+#### Preços diretos com base nacional
+k_n = produto_total_t_pais = produto_total_m_pais <- matrix(0,1,num.paises)
 for (x in 1:num.paises){
   #Produto total em horas de trabalho e em moeda (soma dos setores produtivos)
-  ProdutoTotalTPais[x] <- sum(m.t[lin.produto.total, col.prods[which(pais.cols[col.prods]==x)]])
-  ProdutoTotalMPais[x] <- sum(m.wio[lin.produto.total, col.prods[which(pais.cols[col.prods]==x)]])
+  produto_total_t_pais[x] <- sum(m.t[lin.produto.total, which(pais.cols==x)])
+  produto_total_m_pais[x] <- sum(m.wio[lin.produto.total, which(pais.cols==x)])
   
   #Variável K para o cálculo dos preços diretos (base nacional)
-  k_n[x] <- ProdutoTotalMPais[x]/ProdutoTotalTPais[x]
-  
-  # O FatorDemanda é uma espécide de constate K exclusiva para o consumo das famílias.
-  # Por isso, a utilizei para o cálculo do valor da força de trabalho
-  #FatorDemanda[x] <- DemandaFinalTPais[x]/DemandaFinalMPais[x]
+  k_n[x] <- produto_total_m_pais[x]/produto_total_t_pais[x]
 }
 
 precos_diretos_n <- valores * rep(k_n, each=num.setores)
