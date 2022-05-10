0fa5b7f348fe795f08e8f344040f14f5c06c7423
Rodrigo Borges <rodrigo@borges.net.br>
2022-05-11 00:19:45 +0200

    diregarding depreciation for method zerodep_2

--- R/modules/matrices/wiodr13/disregard_dep.R
+++ R/modules/matrices/wiodr13/disregard_dep.R
@@ -1,8 +1,8 @@
 #Disregards depreciation - initially for course/paper n. 3
 
-m_io[year,"k_depreciation",
+m_io[,"k_depreciation",
      1:nums$input,1:nums$input] <- 0
 
 
 # Calcula as informações setoriais, por país e para o mundo
-sea_sectors[year,"depreciation",,] <- 0
+sea_sectors[,"depreciation",,] <- 0
