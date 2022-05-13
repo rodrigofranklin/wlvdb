84bf7858d2c1a0bd8e0807b7af36771a4026a930
Rodrigo Borges <rodrigo@borges.net.br>
2022-05-13 22:32:27 +0200

    added function to get reference basket

---
+++ R/utils/base_basket_exiobase.R
@@ -0,0 +1,18 @@
+##create base_basket
+base_year <- (dimnames(sea_source)[[1]])[1]
+
+#at this point, corresponding m_io and sea_sectors was already saved, recover it
+
+
+fchnames <- dimnames(m_io)[[4]]
+basebasket <- 
+  m_io[base_year,"consumption_basket",,1]
+
+shc <- substr(dimnames(basebasket)[[2]],1,2)
+basebasket <- array(basebasket,
+                             c(dim(basebasket),1),
+                             dimnames = list(dimnames(basebasket)[[1]],
+                                             shc,
+                                             base_year))
+write_fst_array(consumption_baskets,paste0("results/",method_version,"/base_basket.fst"))
+
