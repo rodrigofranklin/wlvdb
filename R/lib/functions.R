ce80349983431a28e584f9f2346fbe95914090e2
Rodrigo Borges <rodrigo@borges.net.br>
2022-05-13 22:23:38 +0200

    added read_fst_array and write_fst_array functions

--- R/lib/functions.R
+++ R/lib/functions.R
@@ -16,3 +16,38 @@ cnames <- function(a,b) {
   #match column named "names"  from a and b
   match(a$names,b$names)
 }
+
+read_fst_array <- function(file_name) {
+  
+  ft <- fst::read_fst(file_name)  # single column data.frame
+  meta_data <- readRDS(paste0(file_name, ".meta"))  # retrieve dim
+  
+  m <- ft[[1]]  
+  attr(m, "dim") <- meta_data$dim
+  dimensiones <- length(meta_data$dim)
+  meta_data <- meta_data[2:(dimensiones+1)]
+  # lapply(1:dimensiones,function(d,f) {
+  #   dimnames(f)[[d]] <- meta_data[[d]]
+  # })
+  dimnames(m) <- meta_data
+  
+  m
+}
+
+write_fst_array <- function(m, file_name) {
+  
+  # store and remove dims attribute
+  dim <- attr(m, "dim")
+  
+  meta_data <- list(
+    dim = dim
+  )
+  for (i in 1:length(dim)){
+    meta_data[[i+1]] <- dimnames(m)[[i]]
+  }
+  
+  # serialize tale and meta data
+  attr(m, "dim") <- NULL
+  fst::write_fst(data.frame(Data = m), file_name)
+  saveRDS(meta_data, paste0(file_name, ".meta"))
+}
